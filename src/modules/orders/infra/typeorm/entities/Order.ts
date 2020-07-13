d1fc11ba902df67e684df4e6a4cee6173b0ac1ed
Mauricio Blum <mauriciocblum@gmail.com>
2020-07-13 14:40:54 +0100

    initial files

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -4,6 +4,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
+  Column,
   JoinColumn,
   OneToMany,
 } from 'typeorm';
@@ -11,15 +12,26 @@ import {
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => Customer, orderToCostumer => orderToCostumer.id)
   customer: Customer;
 
+  @Column()
+  customer_id: string;
+
+  @OneToMany(() => OrdersProducts, productToOrders => productToOrders.order, {
+    eager: true,
+  })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,41 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Invalid Customer');
+    }
+
+    const productIds = products.map(product => {
+      return { id: product.id };
+    });
+
+    const allProducts = await this.productsRepository.findAllById(productIds);
+
+    const orderProducts = allProducts.map(product => {
+      return {
+        product_id: product.id,
+        quantity: product.quantity,
+        price: product.price,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    return order;
   }
 }
 
